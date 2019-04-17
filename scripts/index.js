ad3f7529c6aaeba9ea120fb9ec33fe4d0d9cb8fe
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-04-17 12:07:25 -0500

    added copy to faq + index + about

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,20 @@
 // const carousels = document.querySelectorAll('.carousel')
-class Accordion{
-    constructor(menu){
-        this.menu = menu
-        this.expand = this.menu.querySelector(".accordion__button")
-        this.expand.addEventListener("click", ()=>{
-        this.select()
-        })
-    }
+class Accordion {
+  constructor(menu) {
+    this.menu = menu;
+    this.expand = this.menu.querySelector(".accordion__button");
+    this.expand.addEventListener("click", () => {
+      this.select();
+    });
+  }
 
-    select(){
-        this.menu.classList.toggle("accordion__show")
-       
-    }
+  select() {
+    this.menu.classList.toggle("accordion__show");
+  }
 }
 
+const accordion = document.querySelectorAll(".accordion");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const accordion = document.querySelectorAll('.accordion')
-
-accordion.forEach((menu)=>{
-
-    return new Accordion(menu)
-})
-
-
+accordion.forEach(menu => {
+  return new Accordion(menu);
+});
